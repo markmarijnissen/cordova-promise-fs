7cfa814fd8bbd941e00dcf24746d7ac63831ea5b
Jan Visser <starquake@linuxeverywhere.org>
2015-04-17 16:29:26 +0200

    Strip trailing slash on dir path
    Strip trailing slash to prevent fs.root.getDirectory in function dir from returning with a file not found error on Windows

--- dist/CordovaPromiseFS.js
+++ dist/CordovaPromiseFS.js
@@ -213,6 +213,12 @@ var CordovaPromiseFS =
 
 	  /* list contents of a directory */
 	  function list(path,mode) {
+
+	    // Strip trailing slash to prevent fs.root.getDirectory in function dir from returning with a file not found error on Windows 
+	    if (path.substr(-1) === '/') {
+	      path = path.substr(0, path.length - 1);
+	    }
+
 	    mode = mode || '';
 	    var recursive = mode.indexOf('r') > -1;
 	    var getAsEntries = mode.indexOf('e') > -1;
