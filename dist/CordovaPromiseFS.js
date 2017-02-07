63d12529aab81f1bba0cc7d64690b162c2fd4422
Mark Marijnissen <markmarijnissen@gmail.com>
2017-02-07 10:30:16 +0100

    1.2.2

--- dist/CordovaPromiseFS.js
+++ dist/CordovaPromiseFS.js
@@ -190,14 +190,7 @@ var CordovaPromiseFS =
 	      // Crosswalk
 	      if(isCrosswalk){
 	        var system = options.fileSystem || 'cachedir';
-	        xwalk.experimental.native_file_system.requestNativeFileSystem(system,function(fs){
-	          fs.root.getDirectory(system, {create: false}, function(dirEntry) {
-	            resolve({
-	              name: fs.name,
-	              root: dirEntry
-	            });
-	          }, reject);
-	        },reject);
+	        xwalk.experimental.native_file_system.requestNativeFileSystem(system,resolve,reject);
 	      // On chrome, request quota to store persistent files
 	      } else if (!isCordova && type === 1 && navigator.webkitPersistentStorage) {
 	        navigator.webkitPersistentStorage.requestQuota(options.storageSize, function(grantedBytes) {
